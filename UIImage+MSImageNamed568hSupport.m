5031e2e66189b8f7ac1a9ba6570d57df3408401d
Hisham A <halshura@gmail.com>
2016-06-07 12:32:09 -0600

    initial iphone 6 support

--- UIImage+MSImageNamed568hSupport.m
+++ UIImage+MSImageNamed568hSupport.m
@@ -8,9 +8,12 @@
 #import "UIImage+MSImageNamed568hSupport.h"
 
 #define kIphone5ScreenHeight 568.0f
+#define kIphone6PlusScreenHeight 736.0f
 #define k568hImageNamePrefix @"-568h"
+#define k736hImageNamePrefix @"-736h"
 
 static BOOL _isIphone5 = NO;
+static BOOL _isIphone6Plus = NO;
 static BOOL _calculatedScreenSize = NO;
 
 @implementation UIImage (MSImageNamed568hSupport)
@@ -23,17 +26,24 @@ static BOOL _calculatedScreenSize = NO;
         const CGFloat screenHeight = MAX(screenSize.width, screenSize.height);
 
         _isIphone5 = (screenHeight == kIphone5ScreenHeight);
+        _isIphone6Plus = (screenHeight == kIphone6PlusScreenHeight);
         _calculatedScreenSize = YES;
     };
 
     UIImage *image = nil;
+    NSString *prefix = nil;
+    if (_isIphone5) {
+        prefix = k568hImageNamePrefix;
+    } else if (_isIphone6Plus) {
+        prefix = k736hImageNamePrefix;
+    }
 
-    if (_isIphone5)
+    if (prefix)
     {
         NSString *imageExtension = [name pathExtension];
         NSString *imageNameWithoutExtension = [name stringByDeletingPathExtension];
 
-        NSString *tallerImageName = [imageNameWithoutExtension stringByAppendingString:k568hImageNamePrefix];
+        NSString *tallerImageName = [imageNameWithoutExtension stringByAppendingString:prefix];
 
         if (imageExtension.length > 0)
         {
